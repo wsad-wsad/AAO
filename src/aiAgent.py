4efb2bc91138e375f021467f513c6f95051b2d38
NotDifined <kakak.jiddan@gmail.com>
2026-01-31 20:03:58 +0700

    0.2.1:udah bisa digunain

--- src/aiAgent.py
+++ src/aiAgent.py
@@ -5,7 +5,6 @@ from typing import Any, Dict
 import dotenv
 from google import genai
 
-# from until.mikir_ai import mikir_ai
 from until.prompt import prompt, prompt_report
 from until.tool import netlas_search  # google_searc
 
@@ -16,21 +15,28 @@ class AiAgent:
     def __init__(self, target):
         self.target = target
 
+    def parse(self, text):
+        return str(text).strip().replace("```json", "").replace("```", "")
+
     def mikir_ai(self) -> Dict[str, Any]:
         client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
         response = client.models.generate_content(
             model="gemini-2.5-flash-lite",
             contents=[prompt.format(target=self.target)],
+            config={
+                "max_output_tokens": 65536,
+            },
         )
-        print(f"[===INFO===]{response.text}")
-        return json.loads(str(response.text))
+        return json.loads(self.parse(response.text))
 
     def scan_target(self):
         result = self.mikir_ai()
 
         if "netlas_search" in result["tools"]:
             return netlas_search(self.target)
+        else:
+            return None
 
     def generate_report(self, data_mentah):
         client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
@@ -38,6 +44,8 @@ class AiAgent:
         response = client.models.generate_content(
             model="gemini-2.5-flash-lite",
             contents=[prompt_report.format(data_mentah=data_mentah)],
+            config={
+                "max_output_tokens": 65536,
+            },
         )
-        print(f"[===INFO REPORT===]{response.text}")
-        return json.loads(str(response.text))
+        return json.loads(self.parse(response.text))
